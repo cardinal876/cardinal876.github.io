f316b5390bcb642cac99d1bb98ce9073f5e3059e
cardinal876 <cardinalfan876@gmail.com>
2025-10-14 15:26:43 -0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const documents = [
-  { id: 1, title: "Apple", content: "A red or green fruit" },
-  { id: 2, title: "Banana", content: "A long yellow fruit" },
-  { id: 3, title: "Cherry", content: "Small red fruit" },
-  { id: 4, title: "Date", content: "Sweet brown fruit" },
-    { id: 5, title: "Mango", content: "Juicy yellow fruit" }
+  { id: 1, title: "Apple", content: "A red or green fruit", url:"result1005.html" },
+  { id: 2, title: "Banana", content: "A long yellow fruit",  url:"result1005.html" },
+  { id: 3, title: "Cherry", content: "Small red fruit",  url:"result1005.html" },
+  { id: 4, title: "Date", content: "Sweet brown fruit",  url:"result1005.html" },
+    { id: 5, title: "Mango", content: "Juicy yellow fruit",  url:"result1005.html" }
 ];
